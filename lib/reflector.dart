1cf469051d779ab0aa6584581f0952e8d2ee8211
bountyforager <bountyforager@gmail.com>
2021-09-17 22:32:31 +0100

    Update reflector.dart

--- lib/reflector.dart
+++ lib/reflector.dart
@@ -17,7 +17,7 @@ class _ReflectorState extends State<Reflector> {
   @override
   void initState() {
     super.initState();
-    print('bountyforagerss');
+    print('bountyforagersss');
 
     // Start listening to changes.
     textInput.addListener(_clearReflection);
